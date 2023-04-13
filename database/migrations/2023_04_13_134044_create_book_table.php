4910a1e236a705b2481099fc2763fafba4f25a56
MaionePietro <pietromaionespam@gmail.com>
2023-04-13 16:23:07 +0200

    first commit

---
+++ database/migrations/2023_04_13_134044_create_book_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book', function (Blueprint $table) {
+            $table->id();
+            $table->string('titolo');
+            $table->tinyInteger('autore_id');
+            $table->tinyInteger('genere_id');
+            $table->tinyInteger('numero_copie');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book');
+    }
+};
