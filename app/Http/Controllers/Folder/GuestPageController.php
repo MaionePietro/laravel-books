798bcc482be145e30d1ca385beb518688055237b
MaionePietro <pietromaionespam@gmail.com>
2023-04-28 16:25:25 +0200

    FEAT validator

--- app/Http/Controllers/Folder/GuestPageController.php
+++ app/Http/Controllers/Folder/GuestPageController.php
@@ -22,11 +22,16 @@ class GuestPageController extends Controller
     }
     public function store(Request $request)
     {
+        $data = $request->validate([
+            'titolo'=>'required|max:255|min:4',  //parametri voluti per verifica
+            'numero_copie'=>'required|integer|min:10',
+            'autore_id'=>'required|integer|min:1'
+        ]);
         $data = $request->all();
         $new_book = new Book();
         $new_book->titolo = $data['titolo'];
         $new_book->autore_id = $data['autore_id'];
-        $new_book->genere_id = $data['genere_id'];
+        $new_book->genere_id = 0;// $data['genere_id'];
         $new_book->numero_copie = $data['numero_copie'];
         $new_book->save();
         return to_route('books.show', $new_book);
@@ -37,10 +42,15 @@ class GuestPageController extends Controller
     }
     public function update(Request $request, Book $book)
     {
+        $data = $request->validate([
+            'titolo'=>'required|string|max:255|min:4',  //parametri voluti per verifica
+            'numero_copie'=>'required|integer|min:10',
+            'autore_id'=>'required|integer|min:1'
+        ]);
         $data = $request->all();
         $book->titolo = $data['titolo'];
         $book->autore_id = $data['autore_id'];
-        $book->genere_id = $data['genere_id'];
+        $book->genere_id = 0 ;//$data['genere_id']
         $book->numero_copie = $data['numero_copie'];
         $book->save();
         return to_route('books.show', $book);
--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -12,25 +12,41 @@
 
             <div class="mb-3">
                 <label for="titolo" class="form-label">Titolo</label>
-                <input type="text" class="form-control" id="titolo" name="titolo" value="{{ old('titolo') }}">
+                <input type="text" class="form-control @error('titolo') is-invalid @enderror" id="titolo" name="titolo"
+                    value="{{ old('titolo') }}">
+                @error('titolo')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label for="autore_id" class="form-label">id autore</label>
-                <input type="text" class="form-control" id="autore_id" name="autore_id"
-                    value="{{ old('autore_id')}}">
+                <input type="text" class="form-control @error('autore_id') is-invalid @enderror" id="autore_id"
+                    name="autore_id" value="{{ old('autore_id') }}">
+                @error('autore_id')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
 
-            <div class="mb-3">
-                <label for="genere_id" class="form-label">genere id</label>
-                <input type="text" class="form-control" id="genere_id" name="genere_id"
-                    value="{{ old('genere_id') }}">
-            </div>
+            {{-- <div class="mb-3"> --}}
+            {{-- <label for="genere_id" class="form-label">genere id</label> --}}
+            {{-- <input type="text" class="form-control" id="genere_id" name="genere_id" --}}
+            {{-- value="{{ old('genere_id') }}"> --}}
+            {{-- </div> --}}
 
             <div class="mb-3">
                 <label for="numero_copie" class="form-label">numero copie</label>
-                <input type="text" class="form-control" id="numero_copie" name="numero_copie"
+                <input type="text" class="form-control @error('numero_copie') is-invalid @enderror" id="numero_copie" name="numero_copie"
                     value="{{ old('numero_copie') }}">
+                    @error('numero_copie')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
 
             <button type="submit" class="btn btn-primary">Salva</button>
